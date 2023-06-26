b5aab45e09ffacd12237d958783e8fa9a2348ca4
vishal.m-at-383439993753 <55.vishalmalusare@gmail.com>
2023-06-26 11:14:03 +0530

    Data binding (input fields)

--- blog/src/components/InputValue.vue
+++ blog/src/components/InputValue.vue
@@ -6,6 +6,33 @@
   <input v-model="password" type="password" placeholder="Enter Password" />
   <br /><br />
   <button v-on:click="getData()" type="button">Get Value</button>
+  <br />
+  <br />
+  <h1>Get checkbox and radio buttons value</h1>
+  <h3>Technology</h3>
+  <label for="java">Java</label>
+  <input type="checkbox" value="java" v-model="technology" id="java" />
+  <label for="JavaScript">JavaScript</label>
+  <input
+    type="checkbox"
+    value="JavaScript"
+    v-model="technology"
+    id="JavaScript"
+  />
+  <label for="PHP">PHP</label>
+  <input type="checkbox" value="PHP" v-model="technology" id="PHP" />
+<br> <br>
+
+<h3>What is your profession</h3>
+<label for="student">Student</label>
+<input type="radio" name="profession" v-model="profession" value="Student" id="student">
+
+<label for="developer">Developer</label>
+<input type="radio" name="profession" v-model="profession" value="Developer" id="developer">
+<br><br>
+  
+  <h4>Selected Technology: {{ technology }}</h4>
+  <h4>Selected Profession: {{ profession }}</h4>
 </template>
 
 <script>
@@ -14,11 +41,13 @@ export default {
     return {
       email: null,
       password: null,
+      technology: [],
+      profession: null,
     };
   },
   methods: {
     getData() {
-      console.log("Values: " ,this.email , this.password);
+      console.log("Values: ", this.email, this.password);
     },
   },
 };
